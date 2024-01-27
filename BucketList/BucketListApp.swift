d50a4d5a1246c3f9fd4ccdeb1f221dc90d94725e
Rob Ranf <rob@emiyaconsulting.com>
2024-01-26 16:44:05 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Rob Ranf on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
